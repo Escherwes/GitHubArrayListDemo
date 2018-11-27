bf0e4b87ed933c4304df10277e0f284bbae9c8d3
Escherwes <45395167+Escherwes@users.noreply.github.com>
2018-11-27 10:14:26 -0500

    Updated GitHub Practicee - New Method
    Added a new method that combines 2 arraylists into one.

--- GitHubPractice.java
+++ GitHubPractice.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
 public class GitHubPractice{
+
+	public static ArrayList combineLists(ArrayList<Integer> list1, ArrayList<Integer> list2){
+
+	}
+
 	public static void main(String[]args){
-	
+
+
+
 	}
 }
