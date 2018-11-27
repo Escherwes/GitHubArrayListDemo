b4feac4db891d459882569d9efd336ba1738ca77
Escherwes <45395167+Escherwes@users.noreply.github.com>
2018-11-27 10:17:17 -0500

    Updated Practice - Completed First Method
    Completed the combineLists method.

--- GitHubPractice.java
+++ GitHubPractice.java
@@ -3,9 +3,24 @@ public class GitHubPractice{
 
 	public static ArrayList combineLists(ArrayList<Integer> list1, ArrayList<Integer> list2){
 
+		ArrayList<Integer> combination = new ArrayList<>();
+
+		for(int i = 0; i<list1.size(); i++){
+			combination.add(list1.get(i));
+		}
+
+		for(int i = 0; i<list2.size(); i++){
+			combination.add(list2.get(i));
+		}
+
+		return combination;
+
 	}
 
 	public static void main(String[]args){
+
+		ArrayList<Integer> list1 = new ArrayList<>();
+		ArrayList<Integer> list2 = new ArrayList<>();
 
 
 
